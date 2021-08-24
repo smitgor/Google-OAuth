0d8b45921ec5f9eb05e50e3a9f23c27e1b128eb2
smitgor <smitgor120@gmail.com>
2021-08-24 22:35:33 +0530

    Srever up and running

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/',(req,res) => {
+    res.send('<h1>Hi there</h1>')
+})
+
+app.listen(port, ()=>{
+    console.log(`Sever is running ${port}`);
+})
